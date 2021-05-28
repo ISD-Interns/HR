43641fc6ad38c05408e000b3be1b6fa44cd846a2
backflip9 <liam.schroth@gmail.com>
2021-05-28 12:45:43 -0700

    ToString() override

--- HaloRuns/HaloRuns/Models/map.cs
+++ HaloRuns/HaloRuns/Models/map.cs
@@ -15,5 +15,10 @@ namespace HaloRuns.Models
         public game Game { get; set; }
         public IEnumerable<run> Runs { get; set; }
 
-    }
+		public override string ToString()
+		{
+            return this.name;
+		}
+
+	}
 }
